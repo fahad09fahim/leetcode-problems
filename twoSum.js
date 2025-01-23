b68f3db2734736d43a55f992ca788529bef2fbb8
fahad09fahim <anik.fahim07@gmail.com>
2025-01-24 00:53:44 +0600

    problem solve twoSum

--- twoSum.js
+++ twoSum.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+function twoSum(nums, target) {
+  const numToIndex = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (complement in numToIndex) {
+      return [numToIndex[complement], i];
+    }
+
+    numToIndex[nums[i]] = i;
+  }
+//   return [];
+}
+
+// Example usage:
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(nums, target);
+console.log(result); // Output: [0, 1]
