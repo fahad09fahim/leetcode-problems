2fcf5e344de84e1546f142c6dc1bdd92dab5c4f2
fahad09fahim <anik.fahim07@gmail.com>
2025-01-24 23:46:13 +0600

    changes method because result 3 got error before

--- isEmpty.js
+++ isEmpty.js
@@ -1,10 +1,12 @@
 let isEmpty = function (obj) {
-  if (obj.length === 0) return true;
+  if (Array.isArray(obj) || typeof obj === "string") {
+    return obj.length === 0;
+  }
   return Object.keys(obj).length === 0;
 };
 const results = isEmpty((obj = [1, 2]));
 const results1 = isEmpty((obj = { x: 5, y: 42 }));
-const results3 = isEmpty((obj = {}));
+const results3 = isEmpty((obj = { length: 0 }));
 console.log(results); // true
 console.log(results1);
 console.log(results3);
